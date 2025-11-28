01512dccfd042d99231c8084f687834be6f84ed1
Leonardo Martinez <leonardo.martinez@mail.mcgill.ca>
2025-11-28 10:51:11 -0500

    changed setup for mimi, we use tr-open-04,tr-open-02,tr-open-03

--- zk/clnt/runclnt.sh
+++ zk/clnt/runclnt.sh
@@ -10,8 +10,8 @@ fi
 
 # TODO Include your ZooKeeper connection string here. Make sure there are no spaces.
 # 	Replace with your server names and client ports.
-export ZKSERVER=localhost:21814,localhost:21815,localhost:21816
-#export ZKSERVER=tr-open-XX.cs.mcgill.ca:218XX,tr-open-XX.cs.mcgill.ca:218XX,tr-open-XX.cs.mcgill.ca:218XX
+#export ZKSERVER=localhost:21814,localhost:21815,localhost:21816
+export ZKSERVER=tr-open-04.cs.mcgill.ca:21814,tr-open-02.cs.mcgill.ca:21814,tr-open-03.cs.mcgill.ca:21814
 # export ZKSERVER=open-gpu-XX.cs.mcgill.ca:218XX,open-gpu-XX.cs.mcgill.ca:218XX,open-gpu-XX.cs.mcgill.ca:218XX
 
 java -cp $CLASSPATH:../task:.: DistClient "$@"
